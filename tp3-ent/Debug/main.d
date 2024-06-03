fbb38216a4d6698607ba7e4d62fc60077c8bde25
Iacobucci Juan B. <link00222@gmail.com>
2024-06-03 15:40:28 -0300

    vterm OK serialport OK
    Terminal Virtual funcional
    Librerías del Puerto Serie funcional
    Cuidado con el Reloj del Atmega

--- tp3-ent/Debug/main.d
+++ tp3-ent/Debug/main.d
@@ -4,7 +4,7 @@ main.d main.o: .././main.c \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
- C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.6.364\include/avr/iom328p.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\iom328p.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
@@ -13,7 +13,8 @@ main.d main.o: .././main.c \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ .././SerialPort.h
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
 
@@ -25,7 +26,7 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
 
-C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.6.364\include/avr/iom328p.h:
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\iom328p.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
 
@@ -44,3 +45,5 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+.././SerialPort.h:
--- tp3-ent/main.c
+++ tp3-ent/main.c
@@ -1,14 +1,46 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
+
 #define F_CPU 16000000UL
 #include <util/delay.h>
 
-int main() {
-	
-	while (1) {
+#include "SerialPort.h"
 
+
+#define BR9600 (0x67)	// 0x67=103 configura BAUDRATE=9600@16MHz
+
+//mensajes de bienvenida y despedida
+char msg1[] = "Hola Mundo, si presiona 's' termina el programa, por favor ingrese una tecla:\n\r";
+char msg2[] = "\n\rHasta luego!";
+
+
+int main(void)
+{
+
+	volatile char dato = 0;
+
+	SerialPort_Init(BR9600); 		// Inicializo formato 8N1 y BAUDRATE = 9600bps
+	SerialPort_TX_Enable();			// Activo el Transmisor del Puerto Serie
+	SerialPort_RX_Enable();			// Activo el Receptor del Puerto Serie
+	SerialPort_Send_String(msg1);   // Envío el mensaje de Bienvenida
+
+	while(1)
+	{
+		SerialPort_Wait_Until_New_Data();	  // Pooling - Bloqueante, puede durar indefinidamente.
+		dato = SerialPort_Recive_Data();
+
+		// Si presionan 's' se termina el programa
+		if( dato == 's')
+		{
+			SerialPort_Send_String(msg2);  // Envío el string de despedida
+			while(1);
+		}
+		else
+		{	// Eco:
+			SerialPort_Wait_For_TX_Buffer_Free(); // Espero a que el canal de transmisión este libre (bloqueante)
+			SerialPort_Send_Data(dato);			  // Envío el dato recibido
+		}
 	}
-	
 	return 0;
 }
 
