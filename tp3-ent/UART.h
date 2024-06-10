22aa85b3c5e86477c057d779d1fe839deede9b0f
Iacobucci Juan B. <link00222@gmail.com>
2024-06-09 22:58:49 -0300

    funciona el parar y seguir de la terminal s S

--- tp3-ent/UART.h
+++ tp3-ent/UART.h
@@ -7,10 +7,10 @@
 #include "SerialPort.h"
 
 void UART_init();
-void UART_sendString(char*);
+void UART_sendString(char* s);
 char UART_hayComando();
 void UART_setHayComando();
-char* UART_getComando();
+char UART_getComando();
 extern volatile int transmision_activa;
 
 #endif /* UART_H_ */
--- tp3-ent/main.c
+++ tp3-ent/main.c
@@ -1,18 +1,13 @@
-
 #include "main.h"
 
-static char bienvenida[]="Registrador de temperatura y humedad \n\r Ingrese s: para reanudar, S para detener,\n\r ";
-static char novalido[]="Comando no valido\n\r";
-volatile int comando=0;
+static char bienvenida[] = "Registrador de temperatura y humedad \n\r Ingrese s: para reanudar, S para detener,\n\r ";
+static char novalido[] = "Comando no valido\n\r";
 volatile int comando_anterior;
-
-//volatile int transmision_activa = 1; // Bandera para indicar si la transmisión está activa (1) o detenida (0)
 
 int main(void){
 
 	UART_init();   // Inicializar UART
 	sei();         // Activar máscara de interrupciones
-	
 	
 	UART_sendString(bienvenida); // Enviar mensaje de inicio/bienvenida
 
@@ -26,17 +21,12 @@ int main(void){
 	DS3231_SetDate(1, 9, 6, 24); // Monday, 09 June 2024 (1 for Monday, 0 for Sunday)
 	
 	while (1){
-		if (UART_hayComando()) {  // Si hay comando nuevo
-			char *comando = UART_getComando();
-			int resultado = Terminal_procesarcomando(comando);
-			if (resultado == 1) {
-				// Detener o reanudar la transmisión según el comando anterior
-				if (comando_anterior == 1) {
-					transmision_activa = 0; // Detener transmisión
-					} else {
-					transmision_activa = 1; // Reanudar transmisión
-				}
-				comando_anterior = 1;
+		if (UART_hayComando()) {
+			char comando = UART_getComando();
+			if (comando == 's') {
+				transmision_activa = 1; // Reanudar transmisión
+				} else if (comando == 'S') {
+				transmision_activa = 0; // Detener transmisión
 				} else {
 				UART_sendString(novalido); // Enviar mensaje de comando no válido
 			}
@@ -48,7 +38,4 @@ int main(void){
 			_delay_ms(2000);
 		}
 	}
-	
-
-
 }
